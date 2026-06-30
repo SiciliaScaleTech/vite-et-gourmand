5820d3b98464955a771dfe6e1ea332ada4003882
Sicilia Toury <sicilia.scaletech@gmail.com>
2026-06-30 11:01:19 +0200

    insertions des documents pdf

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['panier']) && !empty($_SESSION['panier'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vite & Gourmand | Nos menus</title>
+    <title>Vite & Gourmand</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
